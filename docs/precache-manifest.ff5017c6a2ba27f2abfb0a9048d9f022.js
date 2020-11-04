513cc2e189ec8c4dc254e66d8e2fb50bde9d9c97
Todd Turnbull <shingaling@gmail.com>
2020-11-04 14:37:18 -0500

    Build sdc-web with homepage set to full URL in package.json

---
+++ docs/precache-manifest.ff5017c6a2ba27f2abfb0a9048d9f022.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77cdce0f84f75b3ab391619098e00329",
+    "url": "/navigate/index.html"
+  },
+  {
+    "revision": "a1e9d40656602c8e632b",
+    "url": "/navigate/static/js/2.a400c664.chunk.js"
+  },
+  {
+    "revision": "d28ff64ff4908c0da313274d9125c297",
+    "url": "/navigate/static/js/2.a400c664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f136edf7ff1c30f7615",
+    "url": "/navigate/static/js/main.167a3547.chunk.js"
+  },
+  {
+    "revision": "94dc3a1367943627ec9e",
+    "url": "/navigate/static/js/runtime-main.59a1599a.js"
+  },
+  {
+    "revision": "59c4667d1cc392c19661b8923c8eda66",
+    "url": "/navigate/static/media/SocialDevelopmentCentre_Logo_WhiteOnBlack_1.59c4667d.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bc69afca30065dddc51593314147dc0c.js"
+  "/navigate/precache-manifest.ff5017c6a2ba27f2abfb0a9048d9f022.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/navigate/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
