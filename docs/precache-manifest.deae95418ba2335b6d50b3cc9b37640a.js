1c459e1edc45588a0b4a77d99f93a2793255b508
Todd Turnbull <shingaling@gmail.com>
2020-11-04 11:54:21 -0500

    Copy sdc-web/build directory to /docs

---
+++ docs/precache-manifest.deae95418ba2335b6d50b3cc9b37640a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36dd8cdb0c8638dc786f64fc7cc44c0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46cf4993960c26ec2efb",
+    "url": "/static/js/2.7ae2f6bf.chunk.js"
+  },
+  {
+    "revision": "d28ff64ff4908c0da313274d9125c297",
+    "url": "/static/js/2.7ae2f6bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f136edf7ff1c30f7615",
+    "url": "/static/js/main.167a3547.chunk.js"
+  },
+  {
+    "revision": "c54d427c1149912e4264",
+    "url": "/static/js/runtime-main.797d0b2b.js"
+  },
+  {
+    "revision": "59c4667d1cc392c19661b8923c8eda66",
+    "url": "/static/media/SocialDevelopmentCentre_Logo_WhiteOnBlack_1.59c4667d.png"
+  }
+]);
